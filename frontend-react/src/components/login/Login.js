c80ac4353d9f449616c7897a9d7d7306c258ef00
dastagirig1996 <111571182+dastagirig1996@users.noreply.github.com>
2024-09-24 01:53:11 +0530

    updated corsheades

--- frontend-react/src/components/login/Login.js
+++ frontend-react/src/components/login/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axiosInstance.post('http://localhost:8000/customers/login/', {
+      const response = await axiosInstance.post('http://backend:8000/customers/login/', {
         username,
         password,
       });
