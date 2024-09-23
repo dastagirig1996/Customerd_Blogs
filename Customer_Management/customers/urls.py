33e21e5a68645f0e2b9a0dae5a9cc48352665729
dastagirig1996 <111571182+dastagirig1996@users.noreply.github.com>
2024-09-23 22:53:05 +0530

    updated recat

--- Customer_Management/customers/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . views import *
-urlpatterns = [
-    path('customer/', CustomerDetailView.as_view(), name='list_create'),
-    path('customer/<int:id>/', CustomerDetailView.as_view(), name='get_update_delete'),
-    path('login/', LoginView.as_view(), name = "login" ),
-    path('refresh/',refresh_access_token_view,name= "refresh")
-]
